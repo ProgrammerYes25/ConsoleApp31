5b84e33e8985ded309b9937e17766c1ba08b8c22
yes25 <programmer.yes25@gmail.com>
2023-04-20 14:09:26 +0900

    콘솔 제어메서드 (if버전)

--- ConsoleApp31/Program.cs
+++ ConsoleApp31/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp31
@@ -10,6 +11,30 @@ namespace ConsoleApp31
     {
         static void Main(string[] args)
         {
+            int x = 1;
+            while(x < 50){
+                Console.Clear();
+                Console.SetCursorPosition(x, 5);
+
+                if (x % 3 == 0)
+                {
+                    Console.WriteLine("__0");
+
+                }
+                else if (x % 3 == 1)
+                {
+                    Console.WriteLine("_^0");
+                    
+                }
+                else 
+                {
+                    Console.WriteLine("^_0");
+               
+                }
+
+                Thread.Sleep(100);
+                x++;
+            }
         }
     }
 }
